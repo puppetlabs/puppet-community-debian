a07fd64ced796ea141501c09604a00141faa9e4e
Moses Mendoza <moses@puppetlabs.com>
2013-03-07 14:37:35 -0800

    Remove all files

--- lib/puppet/util/windows.rb
+++
@@ -1,7 +0,0 @@
-module Puppet::Util::Windows
-  require 'puppet/util/windows/error'
-  require 'puppet/util/windows/security'
-  require 'puppet/util/windows/user'
-  require 'puppet/util/windows/process'
-  require 'puppet/util/windows/file'
-end
--- spec/integration/file_serving/content_spec.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env rspec
-
-require 'spec_helper'
-
-require 'puppet/file_serving/content'
-
-describe Puppet::FileServing::Content do
-  it_should_behave_like "a file_serving model"
-end
